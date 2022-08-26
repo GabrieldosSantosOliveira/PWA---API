bdd4caf2fe26065655fbfda1b3de417b0c52ac18
Edivaldo <edivaldosantosjunior2004@gmail.com>
2022-08-26 16:28:41 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -9,29 +9,9 @@ function calcular() {
     let peso = document.getElementById("peso").value
     let altura = document.getElementById("altura").value
     let nome = document.getElementById("nome").value = " ";
+
     let result = peso / (altura * altura)
-    let resultado = document.getElementById("nome")
-    document.getElementById("result").innerHTML = result
-
-    result.textContent = Math.floor(result);
-    if (result<18.5) {
-        tabela.textContent = "Abaixo do peso";
-       
-    } else if (result>18.5 & result<24.9) {
-        tabela.textContent = "Peso normal";
-
-    } else if (result>25.0 & result<29.9) {
-        tabela.textContent = "Excesso de peso";
-    
-    } else if (result>30.0 & result<34.9) {
-        tabela.textContent = "Obsidade classe I";
-
-    } else if (result>35.0 & result<39.9) {
-        tabela.textContent = "Obsidade classe II";
-
-    } else if (result>40) {
-        tabela.textContent = "Obsidade classe III";
-    }
+    document.getElementById("result").innerHTML = Math.floor(result);
 
 }
 
