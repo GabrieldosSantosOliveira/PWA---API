84e5c93253dc30f8d386b545e9278b37038c8c7f
Edivaldo <edivaldosantosjunior2004@gmail.com>
2022-08-26 22:18:07 -0400

    Commit

--- js/main.js
+++ js/main.js
@@ -6,13 +6,23 @@ window.onload = () => {
 };
 
 function calcular() {
-    let peso = document.getElementById("peso").value
-    let altura = document.getElementById("altura").value
-    let nome = document.getElementById("nome").value = " ";
+    let peso = document.getElementById("peso").value;
+    let altura = document.getElementById("altura").value;
+    let nome = document.getElementById("nome").value;
+    let idade = document.getElementById("idade").value;
+    let sexo = document.getElementById("sexo").value;
+    let sexo_femi = document.getElementById("sexo_femi").value;
 
     let result = peso / (altura * altura)
     document.getElementById("result").innerHTML = Math.floor(result);
+    imprimir_nome.textContent = nome;
+    imprimir_idade.textContent = idade;
+    imprimir_sexo.textContent = sexo;
+    imprimir_sexo_femi.textContent = sexo_femi;
 
+    if(result > 18){
+        document.getElementById("msg").innerHTML = "gordo";
+    }
 }
 
 
