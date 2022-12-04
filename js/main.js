a27a12ac2aa5bad454b218d0499023bfb338ddd4
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-12-04 13:35:27 -0400

    fix: Call the listing product listing function

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
-window.onload = () => {
-    "use strict";
-    if("serviceWorker" in navigator){
-        navigator.serviceWorker.register("./sw.js");
-    }
+window.onload = async () => {
+  "use strict";
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
+  await APICons();
 };
 
-let api = "https://handsome-spacesuit-bass.cyclic.app/listar-json";
-let url = `https://handsome-spacesuit-bass.cyclic.app/listar-json/${api}`;
-const main = document.querySelector('main');
+let url = `https://handsome-spacesuit-bass.cyclic.app/listar-json`;
+const main = document.querySelector("main");
 
-async function APICons(){
-    const res = await fetch(url);
-    const data = await res.json();
-    main.innerHTML = data.articles.map(createArticle).join('\n');
+async function APICons() {
+  const res = await fetch(url);
+  const data = await res.json();
+  main.innerHTML = data.map(createArticle).join("\n");
 }
-function createArticle(article){
-    return `
+function createArticle(article) {
+  return `
       <div class="article">
       <a href="${article.id}" target="_blank">
          <img src="${article.imagem}"/>
@@ -23,43 +23,49 @@ function createArticle(article){
          <p>${article.preco}</p>
        </a>
    </div>
-    `
+    `;
 }
 
-let posicaoInicial;//variavel para capturar a posicao
-const capturarLocalizacao = document.getElementById('localizacao');
-const latitude = document.getElementById('latitude');
-const longitude = document.getElementById('longitude');
-const map = document.getElementById('mapa');
+let posicaoInicial; //variavel para capturar a posicao
+const capturarLocalizacao = document.getElementById("localizacao");
+const latitude = document.getElementById("latitude");
+const longitude = document.getElementById("longitude");
+const map = document.getElementById("mapa");
 
-const sucesso = (posicao) => {//callback de sucesso para captura da posicao
-    posicaoInicial = posicao;
-    latitude.innerHTML = posicaoInicial.coords.latitude;
-    longitude.innerHTML = posicaoInicial.coords.longitude;
+const sucesso = (posicao) => {
+  //callback de sucesso para captura da posicao
+  posicaoInicial = posicao;
+  latitude.innerHTML = posicaoInicial.coords.latitude;
+  longitude.innerHTML = posicaoInicial.coords.longitude;
 };
 
-const erro = (error) => {//callback de error (falha para captura de localizacao)
-    let errorMessage;
-    switch(error.code){
-        case 0:
-            errorMessage = "Erro desconhecido"
-        break;
-        case 1:
-            errorMessage = "Permissão negada!"
-        break;
-        case 2:
-            errorMessage = "Captura de posição indisponível!"
-        break;
-        case 3:
-            errorMessage = "Tempo de solicitação excedido!" 
-        break;
-    }
-    console.log('Ocorreu um erro: ' + errorMessage);
+const erro = (error) => {
+  //callback de error (falha para captura de localizacao)
+  let errorMessage;
+  switch (error.code) {
+    case 0:
+      errorMessage = "Erro desconhecido";
+      break;
+    case 1:
+      errorMessage = "Permissão negada!";
+      break;
+    case 2:
+      errorMessage = "Captura de posição indisponível!";
+      break;
+    case 3:
+      errorMessage = "Tempo de solicitação excedido!";
+      break;
+  }
+  console.log("Ocorreu um erro: " + errorMessage);
 };
 
-capturarLocalizacao.addEventListener('click', () => {
-    navigator.geolocation.getCurrentPosition(sucesso, erro);
+capturarLocalizacao.addEventListener("click", () => {
+  navigator.geolocation.getCurrentPosition(sucesso, erro);
 
-    map.src = "http://maps.google.com/maps?q="+ posicaoInicial.coords.latitude+"," + posicaoInicial.coords.longitude +"&z=16&output=embed"
-
+  map.src =
+    "http://maps.google.com/maps?q=" +
+    posicaoInicial.coords.latitude +
+    "," +
+    posicaoInicial.coords.longitude +
+    "&z=16&output=embed";
 });
